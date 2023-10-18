f9582e31d1fd67eacb9fb4b7785f5b3d20f5b86b
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2023-10-18 12:23:32 +0200

    clear input.value

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 
 function convert(){
-    const inputValue = document.getElementById("user-number").value
+    let inputValue = document.getElementById("user-number").value
     const userLength = document.getElementById("user-length")
     const userVolume = document.getElementById("user-volume")
     const userMass = document.getElementById("user-mass")
@@ -15,6 +15,8 @@ function convert(){
     userLength.textContent = `${inputValue} meters = ${(inputValue*3.281).toFixed(3)} feet | ${inputValue} feet = ${(inputValue/3.281).toFixed(3)} meters`
     userVolume.textContent = `${inputValue} liters = ${(inputValue*0.264).toFixed(3)} gallons | ${inputValue} gallons = ${(inputValue/0.264).toFixed(3)} liters`
     userMass.textContent = `${inputValue} kilos = ${(inputValue*2.204).toFixed(3)}  pounds | ${inputValue} pounds = ${(inputValue/2.204).toFixed(3)} kilos`
+    inputValue.value = ""
+
   
 }
 

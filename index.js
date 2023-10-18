c31f1bebf229b5009e61f5fbae008f774022226e
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2023-10-18 12:51:53 +0200

    making better

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ function convert(){
     const userLength = document.getElementById("user-length")
     const userVolume = document.getElementById("user-volume")
     const userMass = document.getElementById("user-mass")
+   
 
-  
+
 
     userLength.textContent = `${inputValue.value} meters = ${(inputValue.value*3.281).toFixed(3)} feet | ${inputValue.value} feet = ${(inputValue.value/3.281).toFixed(3)} meters`
     userVolume.textContent = `${inputValue.value} liters = ${(inputValue.value*0.264).toFixed(3)} gallons | ${inputValue.value} gallons = ${(inputValue.value/0.264).toFixed(3)} liters`
     userMass.textContent = `${inputValue.value} kilos = ${(inputValue.value*2.204).toFixed(3)}  pounds | ${inputValue.value} pounds = ${(inputValue.value/2.204).toFixed(3)} kilos`
-    inputValue.value = ""
+    inputValue.value = ""}
 
   
-}
+
 
